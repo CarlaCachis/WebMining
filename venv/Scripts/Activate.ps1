d838fcd9da9b2f4146d5abf9af491977a853e29c
LuisAranaM <luis.arana@pucp.pe>
2019-12-01 21:21:38 -0500

    heroku yolo

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Carla\Desktop\Proyecto\API\venv"
+$env:VIRTUAL_ENV="C:\Users\luisa\Desktop\WebMining\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name

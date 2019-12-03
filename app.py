0db7674e9eb1180b4971725e28957d9e2887e682
LuisAranaM <luis.arana@pucp.pe>
2019-12-02 19:43:42 -0500

    lol

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ class ReusableForm(Form):
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), 
-    	port=int(os.getenv('PORT', 5000)))
+    	port=int(os.getenv('PORT', 80)))
